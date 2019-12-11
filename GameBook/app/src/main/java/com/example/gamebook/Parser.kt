496def34c60d98e80b4ecbca7ed93b1c8e61178e
Luca Ananias <lams3@cin.ufpe.br>
2019-12-10 21:32:19 -0300

    [WIP] Parser for a Game.

---
+++ GameBook/app/src/main/java/com/example/gamebook/Parser.kt
@@ -0,0 +1,22 @@
+package com.example.gamebook
+
+import com.example.gamebook.data.*
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory
+
+object Parser {
+    fun parse(json: String) : Game {
+        val adapter : RuntimeTypeAdapterFactory<Scene> =
+            RuntimeTypeAdapterFactory
+                .of(Scene::class.java)
+                .registerSubtype(TextScene::class.java)
+                .registerSubtype(PasswordScene::class.java)
+                .registerSubtype(ChoiceScene::class.java)
+                .registerSubtype(CameraScene::class.java)
+
+        val gson : Gson = GsonBuilder().setPrettyPrinting().registerTypeAdapterFactory(adapter).create()
+
+        return gson.fromJson(json, Game::class.java)
+    }
+}
