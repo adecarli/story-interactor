c8b6037d7c6583351bdf42edfe92f0f446f38ac7
Luca Ananias <lams3@cin.ufpe.br>
2019-12-11 00:40:31 -0300

    [WIP] NotebookActivity app bar menu

--- GameBook/app/src/main/java/com/example/gamebook/NotebookActivity.kt
+++ GameBook/app/src/main/java/com/example/gamebook/NotebookActivity.kt
@@ -2,11 +2,17 @@ package com.example.gamebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class NotebookActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notebook)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.notebook_app_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
