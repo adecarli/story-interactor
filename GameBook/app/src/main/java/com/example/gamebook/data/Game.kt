2ff8afecb1885eb5f6455d80d81ba954d8818f74
Luca <lams3@cin.ufpe.br>
2019-12-12 17:51:51 -0300

    [WIP] Adding other attributes to Game

--- GameBook/app/src/main/java/com/example/gamebook/data/Game.kt
+++ GameBook/app/src/main/java/com/example/gamebook/data/Game.kt
@@ -1,5 +1,8 @@
 package com.example.gamebook.data
 
 class Game (
-    val scenes : Array<Scene>
+    val title : String,
+    val description : String,
+    val scenes : Array<Scene>,
+    val currentScene: Int = 0
 )
