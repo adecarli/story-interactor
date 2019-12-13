820036611cc5ae76d7b3617765b11bc4d492b42f
Luca <lams3@cin.ufpe.br>
2019-12-13 17:44:40 -0300

    [WIP] Password Activity Done

--- GameBook/app/src/main/java/com/example/gamebook/PasswordActivity.kt
+++ GameBook/app/src/main/java/com/example/gamebook/PasswordActivity.kt
@@ -5,7 +5,18 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gamebook.data.PasswordScene
+import com.example.gamebook.data.TextScene
+import com.example.gamebook.data.database.SerializedGame
+import com.example.gamebook.data.database.SerializedGameDatabase
 import kotlinx.android.synthetic.main.activity_password.*
+import kotlinx.android.synthetic.main.activity_password.button_next
+import kotlinx.android.synthetic.main.activity_password.scene_text
+import kotlinx.android.synthetic.main.activity_password.scene_title
+import kotlinx.android.synthetic.main.activity_text.*
+import org.jetbrains.anko.doAsync
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class PasswordActivity : AppCompatActivity() {
 
@@ -13,9 +24,43 @@ class PasswordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password)
 
-        button_next.setOnClickListener {
-            val intent = Intent(this, CameraActivity::class.java)
-            startActivity(intent)
+        val id = intent.getLongExtra("game_id", -1)
+
+        if (id == -1L)
+            finish()
+
+        val db = SerializedGameDatabase.getInstance(this)
+        doAsync {
+            val dao = db.serializedGameDao()
+            val serializedGame = dao.get(id)!!
+            val game = Parser.fromJson(serializedGame.json)!!
+
+            runOnUiThread {
+                scene_title.text = game.getCurrentScene().title
+                scene_text.text = game.getCurrentScene().text
+            }
+
+            button_next.setOnClickListener {
+                val currentScene = game.getCurrentScene() as PasswordScene
+
+                val time = ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME)
+
+                val actual = password.text.toString()
+                val expected = currentScene.password
+                game.currentSceneIndex = if (actual == expected) currentScene.okLink else currentScene.errorLink
+
+                val newSerializedGame = SerializedGame(serializedGame.uid, Parser.toJson(game), time)
+
+                doAsync {
+                    dao.update(newSerializedGame)
+
+                    runOnUiThread {
+                        val intent = Intent(this@PasswordActivity, game.getCurrentActivity())
+                        intent.putExtra("game_id", id)
+                        startActivity(intent)
+                    }
+                }
+            }
         }
     }
 
