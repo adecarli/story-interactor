aae9e6826b64f52ae6c8c66dcd9f23758500e5e1
Luca <lams3@cin.ufpe.br>
2019-12-13 16:57:08 -0300

    [WIP] TextActivity done

--- GameBook/app/src/main/java/com/example/gamebook/TextActivity.kt
+++ GameBook/app/src/main/java/com/example/gamebook/TextActivity.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gamebook.data.TextScene
+import com.example.gamebook.data.database.SerializedGame
+import com.example.gamebook.data.database.SerializedGameDatabase
 import kotlinx.android.synthetic.main.activity_text.*
+import org.jetbrains.anko.doAsync
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 
 class TextActivity : AppCompatActivity() {
@@ -14,9 +20,37 @@ class TextActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text)
 
-        button_next.setOnClickListener {
-            val intent = Intent(this, ChoiceActivity::class.java)
-            startActivity(intent)
+        val id = intent.getLongExtra("game_id", -1)
+
+        if (id == -1L)
+            finish()
+
+        val db = SerializedGameDatabase.getInstance(this)
+        doAsync {
+            val dao = db.serializedGameDao()
+            val serializedGame = dao.get(id)!!
+            val game = Parser.fromJson(serializedGame.json)!!
+
+            runOnUiThread {
+                scene_title.text = game.getCurrentScene().title
+                scene_text.text = game.getCurrentScene().text
+            }
+
+            button_next.setOnClickListener {
+                val time = ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME)
+                game.currentSceneIndex = (game.getCurrentScene() as TextScene).okLink
+                val newSerializedGame = SerializedGame(serializedGame.uid, Parser.toJson(game), time)
+
+                doAsync {
+                    dao.update(newSerializedGame)
+
+                    runOnUiThread {
+                        val intent = Intent(this@TextActivity, game.getCurrentActivity())
+                        intent.putExtra("game_id", id)
+                        startActivity(intent)
+                    }
+                }
+            }
         }
     }
 
