70b75faea5edc51398db7455fa7ffd62d912ca33
Luca <lams3@cin.ufpe.br>
2019-12-13 18:30:50 -0300

    [WIP] ChoiceActivity done

--- GameBook/app/src/main/java/com/example/gamebook/ChoiceActivity.kt
+++ GameBook/app/src/main/java/com/example/gamebook/ChoiceActivity.kt
@@ -9,7 +9,14 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.setPadding
+import com.example.gamebook.data.ChoiceScene
+import com.example.gamebook.data.database.SerializedGame
+import com.example.gamebook.data.database.SerializedGameDatabase
 import kotlinx.android.synthetic.main.activity_choice.*
+import kotlinx.android.synthetic.main.activity_choice.button_next
+import org.jetbrains.anko.doAsync
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class ChoiceActivity : AppCompatActivity() {
 
@@ -17,23 +24,64 @@ class ChoiceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choice)
 
-        for (i in 1..4) {
-            val radioButton = RadioButton(this)
-            radioButton.text = "Choice $i"
-            radioButton.layoutParams = RadioGroup.LayoutParams(
-                RadioGroup.LayoutParams.MATCH_PARENT,
-                RadioGroup.LayoutParams.WRAP_CONTENT,
-                1f
-            )
-            radioButton.buttonTintList = ColorStateList.valueOf(
-                resources.getColor(R.color.colorPrimary)
-            )
-            radioButton.setTextColor(ColorStateList.valueOf(
-                resources.getColor(R.color.colorPrimaryLight)
-            ))
-            radioButton.setPadding(20)
-            radio_group.addView(radioButton)
+        val id = intent.getLongExtra("game_id", -1)
+
+        if (id == -1L)
+            finish()
+
+        val db = SerializedGameDatabase.getInstance(this)
+        doAsync {
+            val dao = db.serializedGameDao()
+            val serializedGame = dao.get(id)!!
+            val game = Parser.fromJson(serializedGame.json)!!
+
+            val currentScene = game.getCurrentScene() as ChoiceScene
+
+            runOnUiThread {
+                scene_title.text = game.getCurrentScene().title
+                scene_text.text = game.getCurrentScene().text
+
+                for (choice in currentScene.choices) {
+                    val radioButton = RadioButton(this@ChoiceActivity)
+                    radioButton.text = choice.name
+                        radioButton.layoutParams = RadioGroup.LayoutParams(
+                        RadioGroup.LayoutParams.MATCH_PARENT,
+                        RadioGroup.LayoutParams.WRAP_CONTENT,
+                        1f
+                    )
+                    radioButton.buttonTintList = ColorStateList.valueOf(
+                        resources.getColor(R.color.colorPrimary)
+                    )
+                    radioButton.setTextColor(ColorStateList.valueOf(
+                        resources.getColor(R.color.colorPrimaryLight)
+                    ))
+                    radioButton.setPadding(20)
+                    radio_group.addView(radioButton)
+                }
+            }
+
+            button_next.setOnClickListener {
+                val time = ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME)
+
+                game.currentSceneIndex = currentScene.choices.filter { el ->
+                    el.name == radio_group.findViewById<RadioButton>(radio_group.checkedRadioButtonId).text
+                }.first().link
+
+                val newSerializedGame = SerializedGame(serializedGame.uid, Parser.toJson(game), time)
+
+                doAsync {
+                    dao.update(newSerializedGame)
+
+                    runOnUiThread {
+                        val intent = Intent(this@ChoiceActivity, game.getCurrentActivity())
+                        intent.putExtra("game_id", id)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
         }
+
 
         button_next.setOnClickListener {
             val intent = Intent(this, PasswordActivity::class.java)
